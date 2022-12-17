a0f727c4a9cf14b4848312baa4a567238a6930d3
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2022-12-17 21:33:35 +0500

    homepage design commit

--- src/components/CreateProduct.vue
+++ src/components/CreateProduct.vue
@@ -5,11 +5,13 @@
       <form @submit="create">
         <p v-show="success" class="py-2 text-green-600">{{ message }}</p>
         <p v-show="failed" class="py-2 text-red-600">{{ message }}</p>
-        
         <input type="text" placeholder="Product Name" v-model="name"
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-4 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-2">
         <input type="number" step="0.01" placeholder="Product Price" v-model="price"
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-4 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-2">
+        <input type="url" placeholder="Image Url" v-model="image"
+        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-4 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-2">
+
         <button type="submit" class="bg-indigo-500 text-white py-2 px-5 font-semibold rounded-sm">Create</button>
       </form>
     </div>
@@ -24,6 +26,7 @@ export default {
         success: false,
         failed: false,
         message: "",
+        image: "",
         name: "",
         price: 1
       }
@@ -31,7 +34,7 @@ export default {
     methods: {
       async create(e){
         e.preventDefault();
-        if(this.name != "" && this.price > 0 && typeof(this.price) == 'number'){
+        if(this.name != "" && this.price > 0 && typeof(this.price) == 'number' && this.image != ""){
           await fetch("http://localhost:3004/products", {
             method: "post",
             headers: {
@@ -39,7 +42,8 @@ export default {
             },
             body:JSON.stringify ({
               name: this.name,
-              price: this.price
+              price: this.price,
+              product_image: this.image
             })
           }).then((response) => {
               if(response.ok){
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="max-w-4xl m-auto py-6 px-4 w-full">
+    <h1>Home Page</h1>
+    <div class="grid grid-cols-3 gap-3">
+      <div class="bg-gray-100 p-3 rounded-sm w-full" v-for="product in products" :key="product.id">
+          <div class="h-[200px] w-full bg-contain bg-center" style="background-image: url(${{ product.product_image }})"></div>
+          <div class="p-3">
+            <h1 class="font-bold mb-2">{{product.name}}</h1>
+            <p class="text-blue-600 font-bold">$ {{product.price}}</p>
+          </div>
+          <div class="flex items-center">
+            <input type="number" v-model="quantity" placeholder="Quantity" required
+            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-4 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mr-2">
+            <button type="submit" class="bg-gray-900 rounded-md py-2 text-white font-semibold w-full">Add to Cart</button>
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "HomeView"
+    name: "HomeView",
+    data(){
+      return {
+        quantity: 1,
+        products: []
+      }
+    },
+    async created(){
+      const results = await fetch("http://localhost:3004/products");
+      const finalResults = await results.json();
+      this.products = finalResults;
+    }
 }
 </script>
